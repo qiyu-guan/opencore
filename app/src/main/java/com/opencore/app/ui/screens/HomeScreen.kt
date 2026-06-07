22f28e7a24e0814919698cb22fc851e65a3d9dc7
Null-Sector2025 <2734952662@qq.com>
2026-06-07 10:27:03 +0800

    Simplify HomeScreen - remove animation imports

--- app/src/main/java/com/opencore/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/opencore/app/ui/screens/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.opencore.app.ui.screens
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
